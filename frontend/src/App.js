cbeb50e27a237e93553f4f5f7e51fdefa413b82b
Shanice Haye <109613496+shanicenaomihaye@users.noreply.github.com>
2024-08-02 20:57:40 +0100

    Step 3 requirements complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>HELLO WORLD</h1>
-    </div>
+      <p>Hey, I'm Shanice! I loveee all things music. Attending concerts, festivals and small intimate shows is my favourite thing to do in my free time 🎼 I'm very excited about producing our own music-related app!</p>
+      </div>
   );
 }
 
