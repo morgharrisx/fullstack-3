54760ddfa8f62635daec507750550e964be574a6
Mehtap <97293905+mehtapparkinson@users.noreply.github.com>
2024-08-02 17:39:52 +0200

    Step-3 of the requirements done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>HELLO WORLD</h1>
+      <h1>Fullstack Team 3</h1>
+      <p>Hi, I'm Mehtap. I love playing volleyball, especially beach volleyball, which is very popular here in Barcelona. I enjoy staying active and meeting new people through sports </p>
     </div>
   );
 }
