ae13e97cebe036f5710a727a2c7cf6af06aa2237
Laila Alboloushi <laila.sb@outlook.com>
2024-08-03 17:50:45 +0200

    first commit in App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>HELLO WORLD</h1>
+      <p>I'm Laila, I really love the outdoors and being in nature. I try to spend as much time out as possible. Currently training for a marathon on December! </p>
     </div>
   );
 }
