293451c881d5ff847f4529c4b8cbbdb5fd8a8878
zuzannaboguslawska <150298188+zuzannaboguslawska@users.noreply.github.com>
2024-08-07 19:04:00 +0200

    navbar2
    this navbar has logo,name,conacts and login button

--- frontend/src/Navbar/navbar.jsx
+++ frontend/src/Navbar/navbar.jsx
@@ -1,14 +1,11 @@
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
+import {Button, Container, Navbar, Nav } from 'react-bootstrap';
 import './navbar.css'; 
 
-
-function NavBar() {
-    return (
-        <>
-            <Navbar className="custom-bg">
-                <Container>
-                    <Navbar.Brand href="#home" className="our-logo">
+function NavScrollExample() {
+  return (
+    <Navbar expand="lg" className="custom-bg">
+      <Container fluid>
+      <Navbar.Brand href="#home" className="our-logo">
                         <img
                             src="https://storage.googleapis.com/pr-newsroom-wp/1/2023/05/Spotify_Primary_Logo_RGB_Green.png"
                             width="30"
@@ -16,12 +13,21 @@ function NavBar() {
                             className="d-inline-block align-top"
                             alt="Logo" //to be replaced with our one
                         />
-                    </Navbar.Brand>
-                
-                </Container>
-            </Navbar>
-        </>
-    );
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1" className='fw-bold'>VibeFusion</Nav.Link>
+          </Nav>
+            <Nav.Link className='ml-auto mx-3' href="#contact">Contact Us</Nav.Link>
+            <Button variant="success">Log in / Sign up</Button>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
-
-  export default NavBar;
+export default NavScrollExample;
