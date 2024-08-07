5e1e279391c590ff859a170941f08954a62f8087
Laila Alboloushi <laila.sb@outlook.com>
2024-08-07 08:56:04 +0200

    Added button component to index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Button>Button</Button>
   </React.StrictMode>
 );
 
