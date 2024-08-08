4f9084b2e394bbe9eab75c211da72c23cf9bcac8
Morgan Harris <morganharris@live.co.uk>
2024-08-08 12:45:35 +0100

    Add Placeholder image
    Added a placeholder image in place of the album cover, to be replaced when we link the back-end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import ReusableButton from './ReusableButton/ReusableButton';
 import PlaylistItem from './PlaylistItem/PlaylistItem';
 
-
 function App() {
   return (
     <div className="App">
@@ -12,8 +11,10 @@ function App() {
       <PlaylistItem 
         songName="Song Title 1"
         album="Album Name 1"
+        artist="Artist Name 1"
         views="1223"
         runtime="3:45"
+        albumCover={null} // to be replaced with actual album cover url
       />
       </div>
     </div>
--- frontend/src/PlaylistItem/PlaylistItem.jsx
+++ frontend/src/PlaylistItem/PlaylistItem.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
-import { Row, Col, Image } from 'react-bootstrap';
-import '../PlaylistItem/playlistitem.css';
-import musicNoteImage from './images/musicnote.png';
+import { Row, Col, Image, Placeholder } from 'react-bootstrap';
+import './playlistitem.css';
+// import musicNoteImage from './images/musicnote.png';
 
-const PlaylistItem = ({ songName, album, views, runtime }) => {
+const PlaylistItem = ({ songName, album, artist, views, runtime, albumCover }) => {
   return (
     <Row className="playlist-item align-items-center">
       <Col xs={1}>
-        <Image src={musicNoteImage} className="music-note-image" />
+      {albumCover ? (
+          <Image src={albumCover} className="album-cover-image" rounded />
+        ) : (
+          <Placeholder as="div" animation="glow" className="album-cover-placeholder">
+            <Placeholder xs={12} className="placeholder-box"/>
+          </Placeholder>
+        )}
       </Col>
-      <Col xs={4}>
+      <Col xs={3}>
         <span className="song-name">{songName}</span>
       </Col>
-      <Col xs={4}>
+      <Col xs={3}>
         <span className="album-name">{album}</span>
+      </Col>
+      <Col xs={3}>
+      <span className="artist-name">{artist}</span>
       </Col>
       <Col xs={1}>
         <span className="views">{views}</span>
