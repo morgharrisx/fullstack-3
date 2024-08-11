b71b65067a1cf0e626c013b9690310905d4af17c
Shanice Haye <109613496+shanicenaomihaye@users.noreply.github.com>
2024-08-11 11:43:41 +0100

    resolved CORS proxy issue

--- frontend/src/RecommendedSongs/RecommendedSongs.jsx
+++ frontend/src/RecommendedSongs/RecommendedSongs.jsx
@@ -2,3 +2,5 @@ import React, { useEffect, useState } from 'react';
 import PlaylistItem from '../PlaylistItem/PlaylistItem';
 import { Container } from 'react-bootstrap';
 
+const CORS_PROXY = 'https://thingproxy.freeboard.io/fetch/';
+
