1c9f8b3fb0a44d668a112960a12c64ff4b4b7ecf
Morgan Harris <morganharris@live.co.uk>
2024-08-11 17:03:19 +0100

    Effects added to playlist and /recommended page for playlist

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,72 @@
+// import './App.css';
+// import React from 'react';
+// import NavScrollExample from './Navbar/navbar';
+// import ReusableButton from './ReusableButton/ReusableButton';
+// import ControlledCarousel from './Carousel/ControlledCarousel';
+// import BentoGrid from './BentoGrid/BentoGrid';
+// import CardComponent from './Card/Card';
+// // import PlaylistItem from './PlaylistItem/PlaylistItem';
+// import Dashboard from './Dashboard/Dashboard';
+// import RecommendedSongs from './RecommendedSongs/RecommendedSongs';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <NavScrollExample/>
+//       <ControlledCarousel />
+//       <CardComponent header='Header' title='Title' text='This is a random text'></CardComponent>
+//       <ReusableButton color= {'pink'} text='Test First Button'></ReusableButton>
+//       <BentoGrid />
+//       <Dashboard />
+//       <RecommendedSongs />
+//     <Router> 
+//       <Routes>
+//         <Route path="/" element={<App />} />
+//         <Route path="/recommended" element={<RecommendedPlaylist />} />
+//       </Routes>
+//     </Router>
+//      </div>
+//   );
+// }
+
+// export default App;
+
 import './App.css';
+import React from 'react';
 import NavScrollExample from './Navbar/navbar';
 import ReusableButton from './ReusableButton/ReusableButton';
 import ControlledCarousel from './Carousel/ControlledCarousel';
 import BentoGrid from './BentoGrid/BentoGrid';
 import CardComponent from './Card/Card';
-import PlaylistItem from './PlaylistItem/PlaylistItem';
 import Dashboard from './Dashboard/Dashboard';
-import RecommendedSongs from './RecommendedSongs/RecommendedSongs';
+import RecommendedPlaylist from './RecommendedPlaylist'; // Import the RecommendedPlaylist component
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-function App() {
+function App() { // Create a Home component to render your main page layout
   return (
     <div className="App">
       <NavScrollExample/>
       <ControlledCarousel />
       <CardComponent header='Header' title='Title' text='This is a random text'></CardComponent>
-      <ReusableButton color= {'pink'} text='Test First Button'></ReusableButton>
+      <ReusableButton color={'pink'} text='Test First Button'></ReusableButton>
       <BentoGrid />
-      <div className="playlist">
-        <br />
-        <PlaylistItem 
-          songName="Please Please Please"
-          album="Short n' Sweet"
-          artist="Sabrina Carpenter"
-          views="503,795,279"
-          runtime="3:06"
-          albumCover="https://upload.wikimedia.org/wikipedia/en/f/fd/Short_n%27_Sweet_-_Sabrina_Carpenter.png"
-        />
-        <PlaylistItem 
-          songName="CHIHIRO"
-          album="HIT ME HARD AND SOFT"
-          artist="Billie Eilish"
-          views="334,149,206"
-          runtime="5:00"
-          albumCover="https://upload.wikimedia.org/wikipedia/en/a/aa/Billie_Eilish_-_Hit_Me_Hard_and_Soft.png"
-        />
-      </div>
       <Dashboard />
-      <RecommendedSongs />
+      {/* <RecommendedSongs /> */}
     </div>
   );
 }
 
-export default App;
+function Recommended() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/recommended" element={<RecommendedPlaylist />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default Recommended;
