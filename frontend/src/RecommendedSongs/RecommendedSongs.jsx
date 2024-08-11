31131b10de03d4cf8c1483ac27419f81f04bf5f6
Shanice Haye <109613496+shanicenaomihaye@users.noreply.github.com>
2024-08-11 11:44:16 +0100

    created function to convert runtime into mm:ss format

--- frontend/src/RecommendedSongs/RecommendedSongs.jsx
+++ frontend/src/RecommendedSongs/RecommendedSongs.jsx
@@ -4,3 +4,10 @@ import { Container } from 'react-bootstrap';
 
 const CORS_PROXY = 'https://thingproxy.freeboard.io/fetch/';
 
+//Converts runtime to mm:ss format
+const convertToMMSS = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+  };
+
