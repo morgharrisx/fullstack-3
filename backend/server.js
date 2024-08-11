2aaf8161a031f4a3c29bbdf873b11176f82f4107
Laila Alboloushi <laila.sb@outlook.com>
2024-08-11 11:35:45 +0200

    removed token code from app.js frontend to server.js in backend but commented out

--- backend/server.js
+++ backend/server.js
@@ -98,3 +98,26 @@ app.get("/callback", async (req, res) => {
 //     res.send('Error during authentication');
 //   }
 // });
+
+
+// Token and local storage 
+
+    // const [accessToken, setAccessToken] = useState(null);
+    // const [refreshToken, setRefreshToken] = useState(null);
+  
+    // useEffect(() => {
+    //   // Check if there are tokens in the URL
+    //   const urlParams = new URLSearchParams(window.location.search);
+    //   const access_token = urlParams.get('access_token');
+    //   const refresh_token = urlParams.get('refresh_token');
+  
+    //   if (access_token) {
+    //     setAccessToken(access_token);
+    //     localStorage.setItem('access_token', access_token);
+    //   }
+  
+    //   if (refresh_token) {
+    //     setRefreshToken(refresh_token);
+    //     localStorage.setItem('refresh_token', refresh_token);
+    //   }
+    // }, []);
