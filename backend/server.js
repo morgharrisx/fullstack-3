2ad3fce9b84469aa6e267533a32b456f4d994dc4
Laila Alboloushi <laila.sb@outlook.com>
2024-08-12 11:45:59 +0200

    removed redundant code

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 import cors from "cors";
 import express from "express";
-import axios from "axios"; // not using currently 
+import axios from "axios"; // not using currently
 dotenv.config();
 
 import SpotifyWebApi from "spotify-web-api-node";
@@ -20,7 +20,12 @@ const spotifyApi = new SpotifyWebApi({
 
 // route for login authentication
 app.get("/login", (req, res) => {
-  const scopes = ["user-read-private", "user-read-email"];
+  const scopes = [
+    "user-read-private",
+    "user-read-email",
+    "playlist-modify-public",
+    "playlist-modify-private",
+  ];
   res.redirect(spotifyApi.createAuthorizeURL(scopes));
 });
 
@@ -34,8 +39,8 @@ app.get("/callback", async (req, res) => {
   console.log("Error Query Parameter:", error); //showing undefined
 
   if (error) {
-    console.error("Error:", error);
-    res.send(`Error:, ${error}`);
+    console.error("Error during authentication", error);
+    res.send(`Error during authentication:, ${error}`);
     return;
   }
 
@@ -54,7 +59,7 @@ app.get("/callback", async (req, res) => {
         `Access Token:${accessToken}`,
         `Refresh Token:${refreshToken}`
       );
-      res.send("We got it!!");
+      res.send("Success! The callback is working!");
 
       setInterval(async () => {
         const data = await spotifyApi.refreshAccessToken();
@@ -67,57 +72,3 @@ app.get("/callback", async (req, res) => {
       res.send("Error getting token");
     });
 });
-
-
-// The below code is using axios and some frontend connecting (not working atm)
-
-// //callback route for spotify response
-// app.get('/callback', async (req, res) => {
-//   const code = req.query.code || null;
-
-//   try {
-//     const response = await axios({
-//       method: 'post',
-//       url: 'https://accounts.spotify.com/api/token',
-//       data: new URLSearchParams({
-//         grant_type: 'authorization_code',
-//         code: code,
-//         redirect_uri: REDIRECT_URI,
-//       }),
-//       headers: {
-//         'Content-Type': 'application/x-www-form-urlencoded',
-//         'Authorization': 'Basic ' + Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64'),
-//       },
-//     });
-
-//     const { access_token, refresh_token } = response.data;
-
-//     // Redirect to frontend with tokens
-//     res.redirect(`http://localhost:5001?access_token=${access_token}&refresh_token=${refresh_token}`);
-//   } catch (error) {
-//     res.send('Error during authentication');
-//   }
-// });
-
-
-// Token and local storage 
-
-    // const [accessToken, setAccessToken] = useState(null);
-    // const [refreshToken, setRefreshToken] = useState(null);
-  
-    // useEffect(() => {
-    //   // Check if there are tokens in the URL
-    //   const urlParams = new URLSearchParams(window.location.search);
-    //   const access_token = urlParams.get('access_token');
-    //   const refresh_token = urlParams.get('refresh_token');
-  
-    //   if (access_token) {
-    //     setAccessToken(access_token);
-    //     localStorage.setItem('access_token', access_token);
-    //   }
-  
-    //   if (refresh_token) {
-    //     setRefreshToken(refresh_token);
-    //     localStorage.setItem('refresh_token', refresh_token);
-    //   }
-    // }, []);
