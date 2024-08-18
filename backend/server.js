709763bc9769e699c1a594177cbdf7c248d48c5e
Mehtap <97293905+mehtapparkinson@users.noreply.github.com>
2024-08-18 22:39:40 +0200

    average mood of top tracks calculated in /mood endpoint

--- backend/server.js
+++ backend/server.js
@@ -302,5 +302,35 @@ app.get("/me", async (req, res) => {
       return res.status(500).json({ error: 'Failed to fetch user profile' });
     });
     })
+
+
+//CALCULATE MOOD
+app.get('/mood', async (req, res) => {
+  try {
+      const data = await spotifyApi.getMyTopTracks({ limit: 20 });
+      const topTracks = data.body.items;
+      const trackIds = topTracks.map(track => track.id);
+      const audioFeaturesData = await spotifyApi.getAudioFeaturesForTracks(trackIds);
+      const audioFeatures = audioFeaturesData.body.audio_features;
+      console.log('Audio Features:', audioFeatures);
+      let totalValence = 0;
+      let trackCount = 0;
+      for (let index = 0; index < audioFeatures.length; index++) {
+        const feature = audioFeatures[index];
+        if (feature && feature.valence !== undefined) {
+            totalValence += feature.valence;
+            trackCount++;
+        }
+      }
+      const averageValence = trackCount > 0 ? totalValence / trackCount : 0;
+      console.log('Average Valence:', averageValence);
+      res.json({ average_valence: averageValence });
+  } catch (error) {
+      console.error('Error fetching top tracks or audio features:', error);
+      res.status(500).send('Error fetching top tracks or audio features');
+  }
+});
+
+
  
 
