dd38b4ef86720ef2eca294ec78e24769af5b592c
Laila Alboloushi <laila.sb@outlook.com>
2024-08-19 20:05:49 +0200

    fetch for BPM now showing in Treemap in topBPM component

--- frontend/src/DetailedStats/TopBpm/newBPM.jsx
+++ frontend/src/DetailedStats/TopBpm/newBPM.jsx
@@ -2,28 +2,22 @@ import React from "react";
 import { Treemap, ResponsiveContainer } from "recharts";
 import { useEffect, useState } from 'react';
 
-const TopBpm = ({ data, dataKey = "size", ratio = 4 / 3 }) => {
+const TopBpm  = ({ data, dataKey = "size",
+   ratio = 4 / 3 }) => {
 
-  const [bpmData, setBpmData] = useState(null);
-   
+const App = () => {
+      const [bpmData, setBpmData] = useState(placeholderData);
 
   useEffect(() => {
     const fetchBpmData = async () => {
       try {
-        const response = await fetch('http://localhost:5001/detailed-stats');
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
+        const response = await fetch('/detailed-stats');
         const data = await response.json();
-
-        // Log the data to verify it's being fetched correctly
-      console.log("Fetched Data:", data);
 
         // Format the data for the Treemap
         const formattedData = data.map((track) => ({
-          name: (Math.round(track.tempo)),  
-          size: (Math.round(track.tempo)),   
-          
+          name: track.trackName,  // Name of the track (or could use trackName + artistName)
+          size: track.tempo,      // Tempo as the size for the Treemap
         }));
 
         setBpmData(formattedData);
@@ -35,21 +29,17 @@ const TopBpm = ({ data, dataKey = "size", ratio = 4 / 3 }) => {
     fetchBpmData();
   }, []);
 
-  if (!bpmData) {
-    return <p>Loading...</p>; // Display loading message until data is fetched
-  }
-
   return (
     <ResponsiveContainer width="100%" height={300}>
       <p className="lead">Your Top BPM</p>
       <Treemap
-        data={bpmData}
-        dataKey= {dataKey}
+        data={data}
+        dataKey={dataKey}
         ratio={ratio}
         stroke="#fff"
         fill="#FF0080"
       />
-    </ResponsiveContainer> 
+    </ResponsiveContainer>
   );
 };
 
