c92225450a2b3bb70a1bf96021a9bc39b9605ba5
Mehtap <97293905+mehtapparkinson@users.noreply.github.com>
2024-08-21 13:10:36 +0200

    aos zoom animation added to carousel

--- frontend/src/Carousel/ControlledCarousel.jsx
+++ frontend/src/Carousel/ControlledCarousel.jsx
@@ -1,11 +1,19 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import "./carousel.css";
 import { Carousel } from "react-bootstrap";
 import img1 from "./images/1.png";
 import img2 from "./images/2.png";
 import img3 from "./images/3.png";
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+
 
 function ControlledCarousel() {
+  useEffect(() => {
+    AOS.init({ 
+      
+     });
+  }, []);
   const [index, setIndex] = useState(0);
 
   const handleSelect = (selectedIndex) => {
@@ -19,9 +27,12 @@ function ControlledCarousel() {
       className="carousel-container"
       activeIndex={index}
       onSelect={handleSelect}
+      data-aos="zoom in"
+        data-aos-duration="1500"
     >
       {images.map((image, index) => (
-        <Carousel.Item key={index} className="carousel-item">
+        <Carousel.Item  
+        key={index} className="carousel-item">
           <img
             src={image}
             alt={`Slide ${index + 1}`}
